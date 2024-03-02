c9b9069af025db4b942831742d7e7cc22d4c2c3c
ssudemik <sude.emik@stu.fbu.edu.tr>
2024-03-02 11:34:12 +0300

    Category Class

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; } //proporty oluşturma
+        public string CategoryName { get; set; }
+    }
+}
