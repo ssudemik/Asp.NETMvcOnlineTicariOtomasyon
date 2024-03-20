fde1f52eddaebd72f351da98ffaa65c727fd7371
ssudemik <sude.emik@stu.fbu.edu.tr>
2024-03-20 23:13:28 +0300

    layout design

--- MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+using Newtonsoft.Json.Linq;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var values= c.Categories.ToList();
+            return View(values);
+        }
+        [HttpGet] //form(view) yüklendiği zaman bu kısım çalışsın. boş bir sayfa gelsin
+        public ActionResult CategoryAdd()
         {
             return View();
         }
+        [HttpPost] //bir butona tıklandığı zaman bu kısım çalışsın
+        public ActionResult CategoryAdd( Category k)
+        {
+            c.Categories.Add(k); //k nesnesi view tarafından gönderilecek parametreleri tutacak
+            c.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+
     }
 }
